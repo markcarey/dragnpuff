d69c97379d1e29017591d6d5599a11c0de81d3f9
markcarey <mark@markcarey.com>
2024-06-02 09:41:21 -0400

    tweaks

--- firebase/functions/dragn/index.js
+++ firebase/functions/dragn/index.js
@@ -20,9 +20,10 @@ api.get(['/testing'], async function (req, res) {
   //}
 }); // GET /testing
 
-api.get(['/thumbs/:id.png'], async function (req, res) {
+api.get(['/thumbs/:size(1024|512|256|128|64)/:id.png'], async function (req, res) {
   console.log("start GET /thumbs/:id.png path", req.path);
   const id = req.params.id;
+  const size = parseInt(req.params.size);
   const storage = getStorage();
   const bucket = storage.bucket("dragn-puff.appspot.com");
   const file = bucket.file(`images/${id}.png`);
@@ -38,9 +39,8 @@ api.get(['/thumbs/:id.png'], async function (req, res) {
 
   // Generate a thumbnail using sharp.
   const thumbnailBuffer = await sharp(imageBuffer).resize({
-    width: 512,
-    height: 512,
-    withoutEnlargement: true,
+    "width": size,
+    "height": size
   }).toBuffer();
   logger.log("Thumbnail created");
 
@@ -49,5 +49,35 @@ api.get(['/thumbs/:id.png'], async function (req, res) {
     // TODO: Set cache control headers
     res.send(thumbnailBuffer);
 }); // GET /thumbs/:id    
+
+api.get(['/random.png'], async function (req, res) {
+    console.log("start GET /random.png path", req.path);
+    // random id between 1 and 18957
+    const id = Math.floor(Math.random() * 18957) + 1;
+    const storage = getStorage();
+    const bucket = storage.bucket("dragn-puff.appspot.com");
+    const file = bucket.file(`images/${id}.png`);
+    const exists = await file.exists();
+    if (!exists[0]) {
+      res.status(404).send('Not found');
+      return;
+    }
+    // Download file into memory from bucket.
+    const downloadResponse = await file.download();
+    const imageBuffer = downloadResponse[0];
+    logger.log("Image downloaded!");
+  
+    // Generate a thumbnail using sharp.
+    const thumbnailBuffer = await sharp(imageBuffer).resize({
+      width: 512,
+      height: 512
+    }).toBuffer();
+    logger.log("Thumbnail created");
+  
+      // Send the thumbnail in the response.
+      res.set('Content-Type', 'image/png');
+      // TODO: Set cache control headers
+      res.send(thumbnailBuffer);
+  }); // GET /random.png    
 
 module.exports.api = api;
